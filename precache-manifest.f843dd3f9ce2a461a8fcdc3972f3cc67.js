cf0ab24a9b5d9eae70e81f9bae480a0e0de23856
toniktoo <toniktoo@mail.ru>
2020-06-16 11:22:06 +0300

    Updates

---
+++ precache-manifest.f843dd3f9ce2a461a8fcdc3972f3cc67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d20c4fb25abb3ffac31fbca0a8a78380",
+    "url": "/blog-platform-redux-api/index.html"
+  },
+  {
+    "revision": "04ee73360e35d4ca9f60",
+    "url": "/blog-platform-redux-api/static/css/2.a2026422.chunk.css"
+  },
+  {
+    "revision": "f714977bc7abf06b82cc",
+    "url": "/blog-platform-redux-api/static/css/main.de10dc72.chunk.css"
+  },
+  {
+    "revision": "04ee73360e35d4ca9f60",
+    "url": "/blog-platform-redux-api/static/js/2.1ad71249.chunk.js"
+  },
+  {
+    "revision": "4f160e29840e1f64ec2468f8c9029bf3",
+    "url": "/blog-platform-redux-api/static/js/2.1ad71249.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f714977bc7abf06b82cc",
+    "url": "/blog-platform-redux-api/static/js/main.e8a2cdb8.chunk.js"
+  },
+  {
+    "revision": "918b234c8c80e74e7f2e",
+    "url": "/blog-platform-redux-api/static/js/runtime-main.dfafd817.js"
+  }
+]);
